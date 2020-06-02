5ddbc788846621099ecd4770befbf91874bef15e
Shashwat Yadav <shashwatyadav@Shashwats-MacBook-Pro.local>
2020-06-02 11:38:03 +0530

    user change

--- alphaseek/settings.py
+++ alphaseek/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'alphanew',
-        'USER': 'AlphaSeeker',
+        'USER': 'alphaseeker',
         'PASSWORD': 'alpha',
         'HOST': '127.0.0.1',
         'PORT': '5432',

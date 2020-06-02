2bbac4b814ad2e22f84aff3cb2bd369cc4bbb061
Shashwat Yadav <shashwatyadav@Shashwats-MacBook-Pro.local>
2020-06-02 11:53:59 +0530

    Cron Correction

--- alphaseek/settings.py
+++ alphaseek/settings.py
@@ -61,8 +61,8 @@ ROOT_URLCONF = 'alphaseek.urls'
 CORS_ORIGIN_ALLOW_ALL = True
 
 CRONJOBS = [
-    ('2 0 * * *', 'datascrape.cron.daily_nse_pricing_scrape', '>> /home/ec2-user/projects/projectalpha/scheduled_job_daily.log'),
-    ('54 11 2 6 *', 'datascrape.cron.fresh_download_nse_pricing', '>> /home/ec2-user/projects/projectalpha/scheduled_job_fresh.log')
+    ('2 0 * * *', 'pricingdata.cron.daily_nse_pricing_scrape', '>> /home/ec2-user/projects/projectalpha/scheduled_job_daily.log'),
+    ('55 11 2 6 *', 'pricingdata.cron.fresh_download_nse_pricing', '>> /home/ec2-user/projects/projectalpha/scheduled_job_fresh.log')
 ]
 
 CRONTAB_COMMAND_SUFFIX = '2>&1'

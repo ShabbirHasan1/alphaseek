cf62b6aa7306fc2b4d9b9d9a1b5210b7fcd527e7
Shashwat Yadav <shashwatyadav@Shashwats-MacBook-Pro.local>
2020-06-02 12:02:43 +0530

    Check

--- pricingdata/cron.py
+++ pricingdata/cron.py
@@ -2,8 +2,8 @@ from pricingdata import views as data_code
 
 def daily_nse_pricing_scrape():
     print('ran daily_nse_pricing_scrape')
-    data_code.NseIndia.update_all_historic_ticker(partial=False,date_check=True)
+    # data_code.NseIndia.update_all_historic_ticker(partial=False,date_check=True)
 
 def fresh_download_nse_pricing():
     print('ran fresh_download_nse_pricing')
-    data_code.NseIndia.update_all_historic_ticker()
+    # data_code.NseIndia.update_all_historic_ticker()

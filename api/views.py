45653da6c8c993cd5f4d64bb1546377ba1bc6008
yadavshashwat <y.shashwat@gmail.com>
2020-06-27 04:40:22 +0530

    api change

--- api/views.py
+++ api/views.py
@@ -601,19 +601,21 @@ def crud_company_prices(request):
             # outdata = serializers.serialize('python', tranObjs)
             # outdata_sorted = json.dumps([d['fields'] for d in outdata])
             # result['prices']['nse'] = json.loads(outdata_sorted)
-            for trans in tranObjs:       
-                if trans.exchange.exchange_code == "NSE":     
-                    result['prices']['nse'].append({
-                        'date':str(trans.date)[:19]
-                        ,'price_high':trans.price_high
-                        ,'price_low':trans.price_low
-                        ,'price_close':trans.price_close
-                        ,'price_open':trans.price_open
-                        ,'price_close_adjusted':trans.price_open
-                        ,'volume':trans.volume
-                        ,'dividends':trans.dividends
-                        ,'stock_split':trans.stock_split
-                    })
+            sub_nse_data = tranObjs.filter(exchange__exchange_code = "NSE")
+            for trans in sub_nse_data:       
+                # if trans.exchange.exchange_code == "NSE":     
+                result['prices']['nse'].append({
+                    'date':str(trans.date)[:19]
+                    ,'price_high':trans.price_high
+                    ,'price_low':trans.price_low
+                    ,'price_close':trans.price_close
+                    ,'price_open':trans.price_open
+                    ,'price_close_adjusted':trans.price_open
+                    ,'volume':trans.volume
+                    ,'dividends':trans.dividends
+                    ,'stock_split':trans.stock_split
+                })
+
     
     else:
         error = check_operation['error']

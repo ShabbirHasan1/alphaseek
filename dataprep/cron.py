3b59c07ea238eca081dfb4ad92f64239174eab5a
yadavshashwat <y.shashwat@gmail.com>
2020-06-28 21:29:58 +0530

    Changes

--- dataprep/cron.py
+++ dataprep/cron.py
@@ -4,13 +4,15 @@ from datetime import datetime
 def nse_return_calculate():
     print('ran nse_return_calculate')
     timestart = str(datetime.now())
-    print('start time - ' + timestart)
+    print('start time return calculate - ' + timestart)
     clean_data.ReturnCalculate.calculate_all_returns()
     clean_data.ReturnCalculate.calculate_all_index_returns()
+    print('end time return calculate - ' + timestart)
+    print('start time ticker update - ' + timestart)
     clean_data.UpdateTicker.update_all_company()
     clean_data.UpdateTicker.update_all_index()
     timeend = str(datetime.now())
-    print('end time - ' + timeend)
+    print('end time ticker update - ' + timestart)
 
 # def update_company_details():
 #     print('ran update_company_details')

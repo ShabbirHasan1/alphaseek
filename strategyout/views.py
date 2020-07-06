ade18d3c4d9fd4b28a6f161832fd00ed8226a099
yadavshashwat <y.shashwat@gmail.com>
2020-07-07 01:44:48 +0530

    Momentum Crrection

--- strategyout/views.py
+++ strategyout/views.py
@@ -65,21 +65,21 @@ def random_2_asset(update = False):
 def momentum_strategy(months = 6, pickup_percentile = 5, update = False):
     if ((months <= 18) and (months > 0)):
         strategy = CheckStrategy.create_strategy(
-            name="Momentum Strategy | Portfolio update frequency =" + str(months) + " months",
+            name="Long only momentum strategy | Portfolio update frequency =" + str(months) + " months",
             description="Selecting top " + str(pickup_percentile) + " percent assets for the portfolio every " + str(months) + "months. Equi weighted distribution of assets. Long Only for the postions" 
         )
         strategy = strategy['output'][0]
         # logic
         all_monthly_returns = MonthlyReturn.objects.all().order_by('date')
-        months = list(map(lambda x : x.date,all_monthly_returns))
-        months = list(dict.fromkeys(months))
-        total_months = len(months)
+        months_list = list(map(lambda x : x.date,all_monthly_returns))
+        months_list = list(dict.fromkeys(months_list))
+        total_months = len(months_list)
         if not update:
             StrategyPortfolio.objects.filter(strategy = strategy).delete()
 
         for i in range(0,total_months):
-            if ((i) % months):
-                portfolio_options = MonthlyReturn.objects.filter(date = months[i]).order_by("-return_" + str(months) + "m")
+            if (i % months == 0 ):
+                portfolio_options = MonthlyReturn.objects.filter(date = months_list[i]).order_by("-return_" + str(months) + "m")
                 total_options = portfolio_options.count()
                 options_to_select = round(total_options * pickup_percentile / 100 ,0)
                 weight_per_asset = (100/options_to_select)

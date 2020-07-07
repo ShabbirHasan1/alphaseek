ac90df2c68a1553f7adb3b25eb1fb918740aff24
yadavshashwat <y.shashwat@gmail.com>
2020-07-08 03:23:58 +0530

    Code Update

--- check_code.py
+++ check_code.py
@@ -3,4 +3,4 @@ from importlib import reload
 from strategytest.models import *
 
 StrategyDetails.objects.all().delete()
-momentum_strategy(months=6,pickup_percentile=10)
+momentum_strategy(frequency = 1, return_months=12,num_stocks=30)
